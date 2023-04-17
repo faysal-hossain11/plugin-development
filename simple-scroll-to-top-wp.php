5b3815a1b24f05d87d547ffa0da0b0410d9400c5
faysal <you@example.com>
2023-04-17 12:55:41 +0600

    plugin development part start

---
+++ simple-scroll-to-top-wp.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Simple Scroll To Top WP
+ * Plugin URI: https://wordpress.org/plugins/simple-scroll-to-top-wp/
+ * Description: Simple Scroll To Top Plugin will help to you enable Back to top button to your wordpress website.
+ * Version: 1.0.0
+ * Requires al least: 5.2
+ * Requires PHP: 7.2
+ * Author: Faysal hossain
+ * Author URI: https://faysal-hossain11.github.io/portfolio-website/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+*/
+
+?>
